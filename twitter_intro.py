0620fb59c0fa50a5209f4faffd844e7b86d4dee6
krom121 <stephenmacgregor9@gmail.com>
2018-11-01 10:48:59 +0000

    Updated code to accept three locations to find common tweets

--- twitter_intro.py
+++ twitter_intro.py
@@ -13,7 +13,22 @@ auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 DUB_WOE_ID = 560743
+LON_WOE_ID = 44418
+MAN_WOE_ID = 28218
 
 dub_trends = api.trends_place(DUB_WOE_ID)
+lon_trends = api.trends_place(LON_WOE_ID)
+man_trends = api.trends_place(LON_WOE_ID)
 
-print(json.dumps(dub_trends, indent=1))
+dub_trends_set = set([trend['name']
+                    for trend in dub_trends[0]['trends']])
+
+lon_trends_set = set([trend['name']
+                    for trend in lon_trends[0]['trends']])
+
+man_trends_set = set([trend['name']
+                    for trend in man_trends[0]['trends']])                  
+
+common_trends = set.intersection(dub_trends_set, lon_trends_set, man_trends_set)
+
+print(common_trends)
